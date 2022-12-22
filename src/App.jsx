7538270c3eb3ff828127cf4d7cf1a2748904dfba
carlosmilams <118761939+carlosmilams@users.noreply.github.com>
2022-12-22 21:06:12 +0000

    Pending changes exported from your codespace

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 function App() {
+  const name = "StarGazers"
+  const style = {color:"SteelBlue"}
   return (
-    <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-    </hgroup>
+    <article>
+      <hgroup>
+        <img src="images/group.svg" alt="StarGazers Group" />
+        <h1>Meet the <i style={style} >{name}</i></h1>
+        <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+        <button className="outline" onClick={()=> alert("Hi there")}>Click Me!</button>
+      </hgroup>
+    </article>
   )
 }
 export default App
